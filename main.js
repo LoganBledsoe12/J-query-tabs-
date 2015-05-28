d2601be95fedc300aa48fbe90f0df6075e4a42d4
Logan Bledsoe <lbledsoe12@hotmail.com>
2015-05-28 10:26:14 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var $redBox = $('#redbox');
+$redBox.on('click', redbox);
+var $greenBox = $('#greenbox');
+$greenBox.on('click', greenbox);
+var $blueBox = $('#bluebox');
+$blueBox.on('click', bluebox);
+var $bigBox = $('#bigBox');
+
+
+function redbox (){
+	$bigBox.html ('<div><h2>Red</h2></div><br><p>dsfdsfsdfdsfdsfsdfsdfsdfdsfdsjfklsdjfklsdfjlkdsjfklsdjfklsdjfkldsjfklsjdlkf<br>the lorem is not working when i push shit command P so im just going to type up some jabber... hiiiii in class right now your talking about attributes. how are you doing> those brownies you made yesterday were probs the best ive ever had. org in the mouth forsure.sdaskdlasjdalksdjakldjaskldjaskld;jdkljakldsjads;lakdjadlka;jsdl;kadjalksdjaldk;saj</p>');
+}
+function greenbox(){
+	$bigBox.html ('<div><h2>Green</h2></div><br><p>dsfdsfsdfdsfdsfsdfsdfsdfdsfdsjfklsdjfklsdfjlkdsjfklsdjfklsdjfkldsjfklsjdlkfdfjjlksdsklfjskldjflksdjfkljsdlkfjsdklfjsdlkjfklsdjfklsjdfksl;jfs;lkdfjslkdfjsdlkfjsl;dfjds;fljsdfl;sjdfkls;dfjsdklfjsdflkdjsflksjdflksjdflksdjfskldfjslkf;jsdflksjdflk sdjflks dfjklsd jfklsjdf klsdjf ksldjf slkfjs dklfjsd fklsdjf skl;j;klfj sklfg;js fgkljsfdg sdlkjfg lskdgfj gkldjfs glkjgslgkj sd;lkgfj gslk;dj gsklgjsklfgjsklgjfdg dl;kgjfsgkljsdfgkldjgs fgdflgjfsd ;jg;sflkj dgk;lgj sdflgkjsdfglk;gjdslfkgjdfsklgjfdklgjd fsg;lkjd fglkjdsf glksdj</p>');
+}
+function bluebox(){
+	$bigBox.html ('<div><h2>Blue</h2></div><br><p>dsfdsfsdfdsffdskjfklsdjfkldfjslkadjflkjfalkfjdklafjaklfja flkdj fklajf alkdjflkja klaj fklj saklfj dsklsj klaj dfklfjaklfdjsfkl sjdkljf dkljfaskdfajsfkldsja fasdklfj sadlkfjas fsadkljf jsafkdljfskalfj sdfklajf kldasjaklfdjs fklasdjf sakldfj kldfj kl ajdfsklf jflkasdfjda skljfklfjdlkfjaklfjaflkadjsfldasjfal kdflk fajdflkajfkldjsf;ladjfl;kfdj dlkdfj dlkfjads klfajadklfj akldfj a;klsfj a;kls fjdaskl;fjdslkajf akldfjaf dakslfj dsadflk;jaflk;jfakldsjf sal;fjkfj fdkl;sajfkl; dasjflksdajf saldsfsdfsdfsdfdsfdsjfklsdjfklsdfjlkdsjfklsdjfklsdjfkldsjfklsjdlkf</p>');
+}
